29378f65c18cecf1b385a7fe6d256d50819a40b3
Kommit <aksaghovafatima@gmail.com>
2024-01-10 15:16:15 +0300

    Начало

---
+++ telegram_take_dog/src/main/java/com/telegram/telegram_take_dog/TelegramTakeDogApplication.java
@@ -0,0 +1,13 @@
+package com.telegram.telegram_take_dog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelegramTakeDogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TelegramTakeDogApplication.class, args);
+	}
+
+}
---
+++ telegram_take_dog/src/test/java/com/telegram/telegram_take_dog/TelegramTakeDogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.telegram.telegram_take_dog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelegramTakeDogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
